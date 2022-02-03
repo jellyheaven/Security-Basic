4bc36a9850e9de698b7c573f9574ac40ffb365f5
jellyheaven <mayatoto@naver.com>
2022-02-03 15:03:01 +0900

    시큐리티 설정

--- src/main/java/com/h2soft/security1/controller/IndexController.java
+++ src/main/java/com/h2soft/security1/controller/IndexController.java
@@ -2,13 +2,45 @@ package com.h2soft.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 	//mustache 기본 폴더 src/main/resources
 	//뷰리졸버 설정 :/templates(prefix) .mustache(suffix) 생략 가능 application.yml
 	@GetMapping({"","/"})
-	public String index() {
+	public @ResponseBody String index() {
 		return "index";
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	//시큐리티가 낚아챔
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입완료됨.";
 	}
 }
